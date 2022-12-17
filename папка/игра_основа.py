e29a17d9b0fd7094b1d0556382e4762a39e2ba0f
DianaRez <reznikova.da@phystech.edu>
2022-12-17 17:04:48 +0300

    1

--- папка/игра_основа.py
+++ папка/игра_основа.py
@@ -1,5 +1,56 @@
 import pygame
+from pygame.locals import *
+import sys
+import random
+
+class Bachalor:
+    def __init__(self):
+        self.screen = pygame.display.set_mode((800, 600))
+        pygame.font.init()
+        self.score = 0
+        self.direction = 0
+        self.playerx = 400
+        self.playery = 400
+        self.platforms = [[400, 500, 0, 0]]
+        self.cameray = 0
+        self.jump = 0
+        self.gravity = 0
+        self.xmovement = 0
+
+    def updatePlayer(self):
+        if not self.jump:
+            self.playery += self.gravity
+            self.gravity += 1
+        elif self.jump:
+            self.playery -= self.jump
+            self.jump -= 1
+        key = pygame.key.get_pressed()
+        if key[K_RIGHT]:
+            if self.xmovement < 10:
+                self.xmovement += 1
+
+        elif key[K_LEFT]:
+            if self.xmovement > -10:
+                self.xmovement -= 1
+
+        else:
+            if self.xmovement > 0:
+                self.xmovement -= 1
+            elif self.xmovement < 0:
+                self.xmovement += 1
+
+        if self.playerx > 850:
+            self.playerx = -50
+        elif self.playerx < -50:
+            self.playerx = 850
+
+        self.playerx += self.xmovement
+        if self.playery - self.cameray <= 200:
+            self.cameray -= 10
 
 
-def __init__(self):
-    hkgkhb,mb,jgfg b, m
+
+
+
+
+
