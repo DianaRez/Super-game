1dd6553590a15fea0fdcd02b03ac19cfcd808445
taniatdtat <104831305+taniatdtat@users.noreply.github.com>
2022-12-17 18:53:51 +0300

    Create платформы.py

---
+++ папка/платформы.py
@@ -0,0 +1,15 @@
+import pygame
+
+if type_player == 'Girl':
+    player = Girl()
+else:
+    player = Boy()
+
+
+class Bakalavr:
+    def __init__(self):
+        self.screen = pygame.display.set_mode((800, 600))
+        self.green = pygame.draw.rect([400,500,40,40] )
+
+
+
