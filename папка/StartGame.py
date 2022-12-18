c2b117a81b3970c8efdf22233d29838a2c7e6739
DianaRez <reznikova.da@phystech.edu>
2022-12-18 19:07:29 +0300

    1

--- папка/StartGame.py
+++ папка/StartGame.py
@@ -4,8 +4,9 @@ from platforma import *
 import pygame.font
 
 
+
 def StartGame():
-    # player = Girl()
+
     # а вот и музыка
     pygame.mixer.music.load('delta.wav')
     pygame.mixer.music.play(-1)
@@ -19,17 +20,23 @@ def StartGame():
     background = (0, 255, 255)
     num = 1
     display = pygame.display.set_mode((display_width, display_height))  # создаём дисплей игры
-    from Start import print_text
+
+    def text(message, x, y, font_color=(56, 56, 56), font_type='шрифт.ttf', font_size=30):
+        font_type = pygame.font.Font(font_type, font_size)  # создаёт новый объект Font из файла
+        text = font_type.render(message, True,
+                                font_color)  # Вторым аргументом указывается сглаживание, третьим – цвет текста
+        display.blit(text, (x, y))  # выводим на экран в месте с координатами x и y
+
     show = True
     while show:
         display.fill(background)
         Platform.screen.blit(player.image, (player.playerx, player.playery - player.cameray))
         clock.tick(60)
         if num < 5:
-            print_text('{} курс'.format(num), 250, 200, '#FF1493', 'шрифт.ttf', 100)
+            text('{} курс'.format(num), 250, 200, '#FF1493', 'шрифт.ttf', 100)
         else:
-            print_text('Поздравляем, ', 100, 200, '#FF1493', 'шрифт.ttf', 50)
-            print_text('вы окончили физтех!', 80, 300, '#FF1493', 'шрифт.ttf', 50)
+            text('Поздравляем, ', 100, 200, '#FF1493', 'шрифт.ttf', 50)
+            text('вы окончили физтех!', 80, 300, '#FF1493', 'шрифт.ttf', 50)
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT: # если нажали на крестик - программа закрывается
@@ -58,4 +65,3 @@ def StartGame():
         pygame.display.flip()
 
 
-# StartGame()
